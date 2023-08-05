ccbc55497a03c74975055739d78286f424c748fa
kerry <kerry91warnes@gmail.com>
2023-08-05 03:03:37 +0100

    .

--- Program.cs
+++ Program.cs
@@ -7,11 +7,18 @@ namespace CSClass
     {
         static void Main(string[] args)
         {
-            _ = new Employee
-            {
-                firstname = "Fred"
-            };
-            Console.WriteLine("Hello World!");
+            Employee employee1 = new("Fred", "Smith", 25000.00M);
+            Console.WriteLine("Employee1 first name is " + employee1.Firstname);
+            Console.WriteLine("Number of objects is " + Employee.count);
+
+            Employee employee2 = new("Chevy", "Doe", 50000.00M);
+            Console.WriteLine("Employee2 first name is " + employee2.Firstname);
+            Console.WriteLine("Employee2 first name is " + employee2.Lastname);
+            Console.WriteLine("Employee2 first name is " + employee2.Salary);
+
+            Employee employee3 = new("Mic", "Mac", 50000.00M, 23);
+            Console.WriteLine(employee3);
+            Console.WriteLine("Number of objects is " + Employee.count);
         }
     }
 }
--- com/Employee.cs
+++ com/Employee.cs
@@ -1,20 +1,60 @@
 namespace com.hr
 
 /**
-* + String firstname
-* + String lastname
-* + decimal salary
+* - String firstname
+* - String lastname
+* - decimal salary
 */
 
 {
     class Employee
     {
-        public string firstname;
+        private string firstname;
 
-        public string lastname;
+        private string lastname;
 
-        public decimal salary;
+        private decimal salary;
 
         public static int count;
+
+        public string Firstname
+        {get; set;}
+
+        public string Lastname
+        {get; set;}
+
+        public decimal Salary
+        {get; set;}
+
+        public int Years
+        {get; set;}
+
+        public Employee()
+        {
+            count++;
+        }
+
+        public override string ToString()
+        {
+            return "Employee [Firstname = " + Firstname + ", Lastname = "  + Lastname + ", Salary = " + Salary +
+            ", Years = " + Years + "]";
+        }
+
+        public Employee(string firstname, string lastname, decimal salary)
+        {
+            this.Firstname = firstname;
+            this.Lastname = lastname;
+            this.Salary = salary;
+            count++;
+        }
+
+        public Employee(string firstname, string lastname, decimal salary, int years)
+        {
+            this.Firstname = firstname;
+            this.Lastname = lastname;
+            this.Salary = salary;
+            this.Years = years;
+            count++;
+        }
     }
 }
